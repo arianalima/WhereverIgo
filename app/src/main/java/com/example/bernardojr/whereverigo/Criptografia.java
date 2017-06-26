eb020c81fbdfb83afc52966b7c0412e7961dfd6a
Ariana Lima <ariana.hsm@gmail.com>
2017-06-25 23:37:30 -0300

    Login Activity e Criptografia para a senha do usuário

---
+++ app/src/main/java/com/example/bernardojr/whereverigo/Criptografia.java
@@ -0,0 +1,72 @@
+package com.example.bernardojr.whereverigo;
+
+import java.util.HashMap;
+
+public class Criptografia {
+
+    private char[] alfa = new char[52];
+    private char[] nume = new char[10];
+
+    private String letras = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+    private String numeros = "0123456789";
+
+    private HashMap<Character, String> valores = new HashMap<Character, String>();
+    private String senha;
+
+    private static Criptografia criptografia = new Criptografia();
+
+    public static Criptografia getInstancia() {
+        return criptografia;
+    }
+    public void receberSenhaOriginal(String senha){
+        criptografia.setSenhaOriginal(senha);
+    }
+
+    public String getSenhaCriptografada() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < senha.length(); i++) {
+            sb.append(valores.get(senha.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    private void setSenhaOriginal(String senha) {
+        this.senha = senha;
+
+        for (int i = 0; i < 52; i++) {
+            if ((i % 2) == 0) {
+                valores.put(alfa[i], String.format("%02X", logica((senha.length() * i) % 2014)));
+            }
+            else if ((i % 2) != 0) {
+                valores.put(alfa[i], String.format("%02x", logica((senha.length() * i) % 2013)));
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if ((i % 2) == 0) {
+                valores.put(nume[i], String.format("%02X", logica((senha.length() * i) % 2012)));
+            }
+            else if ((i % 2) != 0) {
+                valores.put(nume[i], String.format("%02x", logica((senha.length() * i) % 2011)));
+            }
+        }
+    }
+
+    private long logica(long n) {
+        long cubo = n * n * n;
+
+        long x = cubo + 157;
+        long y = (cubo * n) * (21 * 2007);
+
+        return 2 + x + y;
+    }
+
+    private Criptografia() {
+        for (int i = 0; i < 52; i++) {
+            alfa[i] = letras.charAt(i);
+        }
+        for (int i = 0; i < 10; i ++) {
+            nume[i] = numeros.charAt(i);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bernardojr/whereverigo/LoginActivity.java
@@ -0,0 +1,160 @@
+package com.example.bernardojr.whereverigo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class LoginActivity extends Activity implements View.OnClickListener{
+    private ImageView logo;
+
+    private EditText edtEmail;
+    private EditText edtSenha;
+
+    private Button btnLogin;
+    private Button btnCadastro;
+
+    private Resources resources;
+    private static Context contexto;
+    //private Usuario usuario;
+    //private UsuarioNegocio usuarioNegocio;
+    private Criptografia criptografia;
+    private String senhaCriptografada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        contexto = this;
+        criptografia = Criptografia.getInstancia();
+        logo = (ImageView) findViewById(R.id.appIcon);
+
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
+        btnCadastro = (Button) findViewById(R.id.btn_cadastro);
+        btnCadastro.setOnClickListener(this);
+
+        edtEmail = (EditText) findViewById(R.id.userEmail);
+        edtSenha = (EditText) findViewById(R.id.userSenha);
+
+        initViews();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //usuarioNegocio=UsuarioNegocio.getInstancia(this);
+    }
+
+    private void initViews() {
+        resources = getResources();
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        edtEmail = (EditText) findViewById(R.id.userEmail);
+        edtEmail.addTextChangedListener(textWatcher);
+
+        edtSenha = (EditText) findViewById(R.id.userSenha);
+        edtSenha.addTextChangedListener(textWatcher);
+
+    }
+
+    private boolean validateFields(){
+        String email = edtEmail.getText().toString().trim();
+        String senha = edtSenha.getText().toString();
+        return (!isEmptyFields(email, senha) && isValidEmail(email));
+    }
+
+    private boolean isEmptyFields(String email, String senha) {
+        if (TextUtils.isEmpty(email)) {
+            edtEmail.requestFocus();
+            edtEmail.setError(resources.getString(R.string.campo_vazio));
+            return true;
+        } else if (TextUtils.isEmpty(senha)) {
+            edtSenha.requestFocus();
+            edtSenha.setError(resources.getString(R.string.campo_vazio));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValidEmail(String email) {
+        Pattern regexPattern = Pattern.compile("^[(a-zA-Z-0-9-\\_\\+\\.)]+@[(a-z-A-z)]+\\.[(a-zA-z)]{2,3}$");
+        Matcher regMatcher   = regexPattern.matcher(email);
+        if(regMatcher.matches()) {
+            return true;
+        }
+            return false;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.btn_login:
+                logar(v);
+                break;
+            case R.id.btn_cadastro:
+                startSignUpActivity();
+                break;
+        }
+    }
+
+    private void logar(View view){
+        if (validateFields()){
+            try {
+                String login = edtEmail.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                criptografia.receberSenhaOriginal(senha);
+                senhaCriptografada = criptografia.getSenhaCriptografada();
+
+                //usuario = usuarioNegocio.logar(login, senhaCriptografada);
+                //GuiUtil.exibirSaudacao(this);
+                startNavigationActivity();
+
+            }catch (WhereverIgoException e){
+                //GuiUtil.exibirMsg(LoginActivity.this, e.getMessage());
+
+            }
+        }
+    }
+
+    public void startSignUpActivity() {
+        //Intent i = new Intent(LoginActivity.this,CadastroActivity.class);
+        //startActivity(i);
+    }
+
+    public void startNavigationActivity() {
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
+    }
+
+    public static Context getContexto(){ return contexto; }
+
+}
